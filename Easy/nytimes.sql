c4220cb3b5043d7ee40bd7518e70d96718714690
vandervel <72418762+vandervel@users.noreply.github.com>
2023-05-24 17:38:21 -0700

    added

---
+++ Easy/nytimes.sql
@@ -0,0 +1,6 @@
+/* get viewership for readers on laptop, and for users on mobile (tablet or phone) */
+
+
+select sum(case when device_type = 'laptop' then 1 else 0 end) as laptop_views,
+sum(case when device_type in ('tablet', 'phone') then 1 else 0 end) as mobile_views
+from viewership 
